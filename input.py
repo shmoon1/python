399a2b690cabf56bb16ea23350f94bd86f80b4e5
msh880614 <msh880614@gmail.com>
2019-02-19 23:35:07 +0900

    첫 커밋 ㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎ

---
+++ input.py
@@ -0,0 +1,15 @@
+Python 3.7.2 (tags/v3.7.2:9a3ffc0492, Dec 23 2018, 23:09:28) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> input(1)
+1
+''
+>>> input('마 눌러라')
+마 눌러라ㅇㅇ
+'ㅇㅇ'
+>>> 
+>>> 
+>>> input('눌러봐라\n')
+눌러봐라
+ㅇㅋ
+'ㅇㅋ'
+>>> 
---
+++ test.py
@@ -0,0 +1,78 @@
+Python 3.7.2 (tags/v3.7.2:9a3ffc0492, Dec 23 2018, 23:09:28) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print
+<built-in function print>
+>>> print(1)
+1
+>>> 
+pint
+Traceback (most recent call last):
+  File "<pyshell#2>", line 2, in <module>
+    pint
+NameError: name 'pint' is not defined
+>>> printn(1)
+Traceback (most recent call last):
+  File "<pyshell#3>", line 1, in <module>
+    printn(1)
+NameError: name 'printn' is not defined
+>>> print(123)
+123
+>>> print)1(
+	
+SyntaxError: invalid syntax
+>>> print(1 2) print(!, 2)
+SyntaxError: invalid syntax
+>>> print(!, @, #
+	  
+SyntaxError: invalid syntax
+>>> print(!, @, #)
+	  
+SyntaxError: invalid syntax
+>>> print(1, 2, 3)
+	  
+1 2 3
+>>> print('shmoon')
+	  
+shmoon
+>>> print('My name is', 'hyun')
+	  
+My name is hyun
+>>> my_list = [1,2,3]
+	  
+>>> for i in my_list:
+	  print(l)
+
+	  
+Traceback (most recent call last):
+  File "<pyshell#15>", line 2, in <module>
+    print(l)
+NameError: name 'l' is not defined
+>>> for i in my_list :
+	  print(i)
+
+	  
+1
+2
+3
+>>> import random2
+	  
+Traceback (most recent call last):
+  File "<pyshell#19>", line 1, in <module>
+    import random2
+ModuleNotFoundError: No module named 'random2'
+>>> import random
+	  
+>>> print(ranmdom.randint(1, 10))
+	  
+Traceback (most recent call last):
+  File "<pyshell#21>", line 1, in <module>
+    print(ranmdom.randint(1, 10))
+NameError: name 'ranmdom' is not defined
+>>> 
+	  
+>>> import random
+	  
+>>> print(random.randint(1,10))
+	  
+2
+>>> 
